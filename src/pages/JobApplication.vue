1da2f048fc7354395604918d09aca55b0d7f2340
Zignuts Parth Gupta <parthg@zignuts.com>
2024-04-24 13:09:03 +0530

    listing of job application is done

--- src/pages/JobApplication.vue
+++ src/pages/JobApplication.vue
@@ -1,3 +1,435 @@
+<script setup>
+// import necessary modules and components
+import { avatarText } from "@/@core/utils/formatters";
+import AddNewJobApplicationDrawer from "../views/AddNewJobApplicationDrawer.vue";
+import { onMounted, ref } from "vue";
+import { VDataTable } from "vuetify/labs/VDataTable";
+import axios from "../axiosFile.js";
+import { computed } from "vue";
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+import { debounce } from "lodash";
+
+// Reactive variables and references
+const deleteDialog = ref(false);
+const isAddNewCompanyDrawerVisible = ref(false);
+const editCompanyData = ref(null);
+const isEditMode = ref(false);
+const deleteItemId = ref(null);
+const userList = ref([]);
+const permentDelete = ref(false);
+const tempDelete = ref(false);
+const loading = ref(false);
+const search = ref("");
+const items = [
+  "Approved",
+  "Pending",
+  "Rejected",
+];
+const selectedFilter = ref(null);
+
+// headers
+const headers = [
+  {
+    title: "Application No",
+    key: "application_id",
+  },
+  {
+    title: "Name of Candidate",
+    key: "candidate_name",
+  },
+  {
+    title: "Company Applied In",
+    key: "company_name",
+  },
+  {
+    title: "Postion Applied for",
+    key: "job_title",
+  },
+  {
+    title: "Resume",
+    key: "resume_path",
+  },
+  {
+    title: "Status",
+    key: "status",
+  },
+  {
+    title: "Actions",
+    key: "actions",
+  },
+];
+
+// status 
+const resolveStatus = (status) => {
+  switch (status) {
+    case 'P':
+      return { text: 'Pending', color: 'secondary' }; 
+    case 'R':
+      return { text: 'Rejected', color: 'error' }; 
+    case 'A':
+      return { text: 'Approved', color: 'success' }; 
+    default:
+      return { text: 'Unknown', color: 'grey' }; 
+  }
+};
+
+// Function to open add new job drawer
+const openAddNewCompanyDrawer = async (employeeData) => {
+  console.log(employeeData);
+  if (employeeData) {
+    try {
+      const response = await axios.get(`/job/${employeeData.id}`);
+      editCompanyData.value = response.data;
+      if (editCompanyData.value) {
+        isEditMode.value = true;
+        isAddNewCompanyDrawerVisible.value = true;
+      }
+    } catch (error) {
+      console.error("Failed to fetch employee details:", error.message);
+    }
+  } else {
+    editCompanyData.value = null;
+    isEditMode.value = false;
+    isAddNewCompanyDrawerVisible.value = true;
+  }
+};
+
+// Function to delete item
+const deleteItem = (item) => {
+  deleteItemId.value = item;
+  deleteDialog.value = true;
+};
+
+// Function to close delete dialog
+const closeDelete = () => {
+  deleteDialog.value = false;
+  permentDelete.value = false;
+  tempDelete.value = false;
+};
+
+// Computed property to check if submit button is enabled
+const isSubmitEnabled = computed(() => permentDelete.value || tempDelete.value);
+
+// Function to confirm deletion of item
+const deleteItemConfirm = async () => {
+  if (isSubmitEnabled.value) {
+    try {
+      const token = localStorage.getItem("token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const params = {
+        permanent_delete: permentDelete.value,
+      };
+
+      // Update the delete request to include the token in the headers
+      await axios.post(`/job/delete/${deleteItemId.value}`, null, {
+        ...config,
+        params,
+      });
+
+      userList.value = userList.value.filter(
+        (employee) => employee.id !== deleteItemId.value
+      );
+
+      closeDelete();
+      toast("Job deleted successfully", {
+        theme: "auto",
+        type: "success",
+        pauseOnHover: false,
+        pauseOnFocusLoss: false,
+        dangerouslyHTMLString: true,
+      });
+    } catch (error) {
+      toast("Failed to delete Job", {
+        theme: "auto",
+        type: "error",
+        pauseOnHover: false,
+        pauseOnFocusLoss: false,
+        dangerouslyHTMLString: true,
+      });
+    }
+  }
+};
+// Function to handle new job data
+const handleNewUserData = async (employeeData) => {
+  try {
+    loading.value = true;
+    if (isEditMode.value) {
+      let response = await axios.post(
+        `/job/update/${editCompanyData.value.id}`,
+        employeeData
+      );
+      if (response) {
+        toast("Job Updated successfully", {
+          theme: "auto",
+          type: "success",
+          pauseOnHover: false,
+          pauseOnFocusLoss: false,
+          dangerouslyHTMLString: true,
+        });
+      }
+    } else {
+      let response = await axios.post("job/create", employeeData);
+      if (response) {
+        toast("Job Created Successfully", {
+          theme: "auto",
+          type: "success",
+          pauseOnHover: false,
+          pauseOnFocusLoss: false,
+          dangerouslyHTMLString: true,
+        });
+      }
+    }
+
+    fetchData();
+
+    isAddNewCompanyDrawerVisible.value = false;
+    loading.value = false;
+  } catch (error) {
+    console.error("Failed to update or create Employee:", error.message);
+    loading.value = false;
+  }
+};
+
+// Function to fetch data call an api
+const fetchData = async () => {
+  loading.value = true;
+  try {
+    const response = await axios.get("/allCandidateInfo");
+    userList.value = response.data;
+    console.log(userList.value);
+  } catch (error) {
+    console.error("Failed to Candidate data:", error.message);
+  }
+  loading.value = false;
+};
+
+// Function to handle delete option change
+const handleDeleteOptionChange = (option) => {
+  if (option === "permanent") {
+    tempDelete.value = false;
+  } else if (option === "temporary") {
+    permentDelete.value = false;
+  }
+};
+
+// Debounced search function
+const debouncedSearch = debounce(() => {
+  console.log("Searching for:", search.value);
+}, 500);
+
+// Watcher for search input
+watch(
+  [search, selectedFilter],
+  async ([searchValue, filterValue], [prevSearchValue, prevFilterValue]) => {
+    // Check if search value or filter value has changed
+    if (searchValue !== prevSearchValue || filterValue !== prevFilterValue) {
+      try {
+        const token = localStorage.getItem("token");
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+
+        let apiUrl = "/jobs";
+        const params = {};
+
+        if (searchValue) {
+          params.search = searchValue;
+        }
+
+        if (filterValue) {
+          params.employment_type = filterValue;
+        }
+
+        const response = await axios.get(apiUrl, { params, ...config });
+        userList.value = response.data;
+      } catch (error) {
+        console.error("Failed to fetch job data:", error.message);
+      }
+    }
+  },
+  { deep: true } // Set deep option to true to watch nested properties
+);
+
+// Fetch data on component mount
+onMounted(() => {
+  fetchData();
+});
+</script>
+
+<!-- Template section -->
 <template>
-    <h1>hello</h1>
+  <div>
+    <div v-if="loading" class="d-flex justify-center">
+      <VProgressCircular :size="40" color="primary" indeterminate />
+    </div>
+    <div v-else>
+      <div class="d-flex justify-end ma-3">
+        <VBtn prepend-icon="tabler-plus" @click="openAddNewCompanyDrawer(null)">
+          Create New Job
+        </VBtn>
+      </div>
+
+      <!-- search bar -->
+      <div class="search-container d-flex align-center">
+        <div class="search-input" style="width: 70%">
+          <VTextField
+            v-model="search"
+            label="Search"
+            outlined
+            dense
+            clearable
+            placeholder="Search Jobs by Title"
+          />
+        </div>
+        <!-- filter search -->
+        <div class="filter-select" style="width: 30%">
+          <AppSelect
+            v-model="selectedFilter"
+            :items="items"
+            clearable
+            clear-icon="tabler-x"
+            single-line
+            placeholder="Show By Type"
+          />
+        </div>
+      </div>
+
+      <!-- data table -->
+      <VDataTable :headers="headers" :items="userList" :items-per-page="10">
+        <template #item.application_id="{ item }">
+          <span>{{ item.raw.application_id }}</span>
+        </template>
+      
+        <template #item.candidate_name="{ item }">
+          <div class="d-flex align-center">
+            <VAvatar
+              size="32"
+              :color="item.raw.avatar ? '' : 'primary'"
+              :class="item.raw.avatar ? '' : 'v-avatar-light-bg primary--text'"
+              :variant="!item.raw.avatar ? 'tonal' : undefined"
+            >
+              <span>{{ avatarText(item.raw.candidate_name) }}</span>
+            </VAvatar>
+      
+            <div class="d-flex flex-column ms-3">
+              <span
+                class="d-block font-weight-medium text--primary text-truncate"
+                >{{ item.raw.candidate_name }}</span
+              >
+            </div>
+          </div>
+        </template>
+      
+        <template #item.company_name="{ item }">
+          <span>{{ item.raw.company_name }}</span>
+        </template>
+      
+        <template #item.job_title="{ item }">
+          <span>{{ item.raw.job_title }}</span>
+        </template>
+      
+        <template #item.resume_path="{ item }">
+          <a :href="`http://127.0.0.1:8000/storage/${item.raw.resume_path}`" target="_blank" rel="noopener noreferrer">Download</a>
+        </template>
+      
+        <template #item.status="{ item }">
+          <VChip
+            :color="resolveStatus(item.raw.status).color"
+            size="small"
+            label
+            class="text-capitalize"
+          >
+            <span>{{ resolveStatus(item.raw.status).text }}</span>
+          </VChip>
+        </template>
+      
+        <template #item.actions="{ item }">
+          <div class="d-flex gap-1">
+            <IconBtn @click="openAddNewCompanyDrawer(item.raw)">
+              <VIcon icon="mdi-pencil-outline" />
+            </IconBtn>
+            <IconBtn @click="deleteItem(item.raw.application_id)">
+              <VIcon icon="mdi-delete-outline" />
+            </IconBtn>
+          </div>
+        </template>
+      </VDataTable>
+    </div>
+    <!-- dialog to delete the items -->
+    <VDialog v-model="deleteDialog" max-width="500px">
+      <VCard>
+        <VCardTitle class="text-center d-flex align-center justify-center mb-3">
+          Are you sure you want to delete this item?
+        </VCardTitle>
+        <div class="text-center d-flex align-center justify-center mb-5">
+          <VCheckbox
+            v-model="permentDelete"
+            label="DELETE ITEM PERMANENTLY"
+            name="del"
+            @change="handleDeleteOptionChange('permanent')"
+          />
+        </div>
+        <div class="text-center d-flex align-center justify-center mb-5">
+          <VCheckbox
+            v-model="tempDelete"
+            label="DELETE ITEM TEMPORARILY"
+            name="del"
+            @change="handleDeleteOptionChange('temporary')"
+          />
+        </div>
+        <VCardActions>
+          <VSpacer />
+          <VBtn color="error" variant="outlined" @click="closeDelete">
+            Cancel
+          </VBtn>
+          <VBtn
+            color="success"
+            variant="elevated"
+            @click="deleteItemConfirm"
+            :disabled="!isSubmitEnabled"
+          >
+            OK
+          </VBtn>
+          <VSpacer />
+        </VCardActions>
+      </VCard>
+    </VDialog>
+    <!-- job drawer -->
+    <AddNewJobApplicationDrawer
+      v-model:isEmployeeDrawerOpen="isAddNewCompanyDrawerVisible"
+      :employee-data="editCompanyData"
+      @employee-data="handleNewUserData"
+    />
+  </div>
 </template>
+
+<!-- Scoped styles -->
+<style scoped>
+.search-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+
+.search-input {
+  width: 70%;
+  margin-right: 10px;
+}
+
+.filter-select {
+  width: 30%;
+}
+
+.v-text-field {
+  width: 100%;
+}
+</style>
---
+++ src/views/AddNewJobApplicationDrawer.vue
@@ -0,0 +1,352 @@
+<script setup>
+// Import necessary validators
+import { requiredValidator, emailValidator } from "@validators";
+import { defineProps, defineEmits, onMounted, ref, watch, nextTick } from "vue";
+import { PerfectScrollbar } from "vue3-perfect-scrollbar";
+import axios from "../axiosFile";
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+
+// Define props and emits
+const props = defineProps({
+  isEmployeeDrawerOpen: {
+    type: Boolean,
+    required: true,
+  },
+  employeeData: {
+    type: Object,
+    default: null,
+  },
+});
+const emit = defineEmits(["update:isEmployeeDrawerOpen", "employeeData"]);
+
+// Define reactive variables
+const companyOptions = ref([]);
+const isFormValid = ref(false);
+const refForm = ref();
+const Title = ref("");
+const Salary = ref("");
+const EmploymentStatus = ref("");
+const ExperienceRequired = ref("");
+const PostedOn = ref(null);
+const ExpiresOn = ref(null);
+const CompanyId = ref(null);
+const skillsRequired = ref([]);
+
+// Function to fetch company names from API
+const fetchCompanyNames = async () => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get("getallcompanies");
+    companyOptions.value = response.data;
+  } catch (error) {
+    console.error("Error fetching company options:", error);
+  }
+};
+
+// Function to handle model value update
+const handleDrawerModelValueUpdate = (val) => {
+  emit("update:isEmployeeDrawerOpen", val);
+};
+
+// Function to clear the form
+const clearForm = () => {
+  refForm.value?.reset();
+  PostedOn.value = "";
+  ExpiresOn.value = "";
+  skillsRequired.value = [];
+  refForm.value?.resetValidation();
+};
+
+// Watch for changes in employeeData prop
+watch(
+  () => props.employeeData,
+  (newValue) => {
+    if (newValue) {
+      Title.value = newValue.title;
+      Salary.value = newValue.salary;
+      EmploymentStatus.value = newValue.employment_type;
+      ExperienceRequired.value = newValue.experience_required;
+      PostedOn.value = newValue.posted_date;
+      ExpiresOn.value = newValue.expiry_date;
+      CompanyId.value = newValue.company_id;
+      skillsRequired.value = newValue.skills_required
+        ? newValue.skills_required.split(",").map((skill) => skill.trim())
+        : [];
+    } else {
+      CompanyId.value = null;
+      clearForm();
+    }
+  }
+);
+watch(
+  () => skillsRequired.value,
+  (newValue) => {
+    if (newValue.includes("No Specific Skills Required") && newValue.length > 1) {
+      skillsRequired.value = newValue.filter(skill => skill !== "No Specific Skills Required");
+    }
+  }
+);
+const skillsOptions = [
+  { label: "MERN", value: "MERN" },
+  { label: "MEAN", value: "MEAN" },
+  { label: "LARAVEL+VUE", value: "LARAVEL+VUE" },
+  { label: "FLUTTER", value: "FLUTTER" },
+  { label: "DEVOPS", value: "DEVOPS" },
+  { label: "UI / UX", value: "UI / UX" },
+  { label: "ANDROID", value: "ANDROID" },
+  { label: "SALESFORCE", value: "SALESFORCE" },
+  { label: "REACT.JS", value: "REACT.JS" },
+  { label: "NODE.JS", value: "NODE.JS" },
+  { label: "AWS", value: "AWS" },
+  { label: "DBA", value: "DBA" },
+  {
+    label: "No Specific Skills Required",
+    value: "No Specific Skills Required",
+  },
+];
+
+const onSubmit = async () => {
+  try {
+    let validate = await refForm.value?.validate();
+    if (validate.valid) {
+      // Form is valid, proceed with submission
+      if (new Date(ExpiresOn.value) < new Date(PostedOn.value)) {
+        // Validation for specific condition
+        toast("Joining Date cannot be before Date of Birth", {
+          theme: "auto",
+          type: "error",
+          pauseOnHover: false,
+          pauseOnFocusLoss: false,
+          dangerouslyHTMLString: true,
+        });
+        return;
+      }
+      if (Salary.value > 1000000) {
+        // Validation for specific condition
+        toast("Enter the Valid value for Salary", {
+          theme: "auto",
+          type: "error",
+          pauseOnHover: false,
+          pauseOnFocusLoss: false,
+          dangerouslyHTMLString: true,
+        });
+        return;
+      }
+      if (skillsRequired.value.length === 0) {
+        // Validation for specific condition
+        toast("Please select at least one skill", {
+          theme: "auto",
+          type: "error",
+          pauseOnHover: false,
+          pauseOnFocusLoss: false,
+          dangerouslyHTMLString: true,
+        });
+        return;
+      }
+      // All validations passed, proceed with form submission
+      const formData = {
+        title: Title.value,
+        salary: Salary.value,
+        employment_type: EmploymentStatus.value,
+        experience_required: ExperienceRequired.value,
+        posted_date: PostedOn.value,
+        expiry_date: ExpiresOn.value,
+        company_id: CompanyId.value,
+        skills_required: skillsRequired.value.join(", "),
+      };
+      console.log(formData);
+      emit("employeeData", formData);
+      closeNavigationDrawer();
+      nextTick(() => {
+        clearForm();
+      });
+    } else {
+      // Form is invalid, display a message indicating required fields
+      toast("Please fill in all required fields", {
+        theme: "auto",
+        type: "error",
+        pauseOnHover: false,
+        pauseOnFocusLoss: false,
+        dangerouslyHTMLString: true,
+      });
+    }
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+};
+
+
+// Function to close the navigation drawer
+const closeNavigationDrawer = () => {
+  emit("update:isEmployeeDrawerOpen", false);
+  clearForm();
+};
+
+// Fetch company names from API on component mount
+onMounted(fetchCompanyNames);
+</script>
+
+<template>
+  <div>
+  <VNavigationDrawer
+    temporary
+    :width="400"
+    location="end"
+    class="scrollable-content"
+    :model-value="props.isEmployeeDrawerOpen"
+    @update:model-value="handleDrawerModelValueUpdate"
+  >
+    <!-- Title -->
+    <AppDrawerHeaderSection
+      :title="props.employeeData ? 'Edit Job' : 'Add Job'"
+      @cancel="closeNavigationDrawer"
+    />
+
+    <PerfectScrollbar :options="{ wheelPropagation: false }">
+      <VCard flat>
+        <VCardText>
+          <VForm ref="refForm" v-model="isFormValid" @submit.prevent="onSubmit">
+            <VRow>
+              <!-- Title -->
+              <VCol cols="12">
+                <AppTextField
+                  v-model="Title"
+                  :rules="[requiredValidator]"
+                  label="Title"
+                />
+              </VCol>
+
+              <!-- Salary -->
+              <VCol cols="12">
+                <AppTextField
+                  v-model="Salary"
+                  type="number"
+                  label="Salary"
+                  placeholder="Salary Valid Upto 1000000"
+                />
+              </VCol>
+
+              <!-- Employment Status -->
+              <VCol cols="12">
+                <AppSelect
+                  v-model="EmploymentStatus"
+                  :items="[
+                    'Full time',
+                    'Part time',
+                    'Hybrid',
+                    'Work from Home',
+                    'Work From Office',
+                    'Internship',
+                  ]"
+                  label="Employment Status"
+                  placeholder="Employment Status"
+                />
+              </VCol>
+
+              <!-- Experience Required -->
+              <VCol cols="12">
+                <AppSelect
+                  v-model="ExperienceRequired"
+                  :items="[
+                    'Low Level (0 to 1 Yrs) ',
+                    'Medium Level (2 TO 5 Yrs)',
+                    'High Level (5+ Yrs)',
+                  ]"
+                  label="Experience Required"
+                  placeholder="Experience Required"
+                />
+              </VCol>
+
+              <!-- skills required -->
+              <VCol cols="12">
+                <label for="skillsRequired" class="skills-margin"
+                  >Skills Required</label
+                >
+                <div id="skillsRequired" class="skills-grid">
+                  <!-- List of checkboxes for skills -->
+                  <div
+                    v-for="skill in skillsOptions"
+                    :key="skill.value"
+                    class="skills-checkbox"
+                  >
+                    <VCheckbox
+                      v-model="skillsRequired"
+                      :value="skill.value"
+                      :label="skill.label"
+                      :id="skill.value"
+                    ></VCheckbox>
+                  </div>
+                </div>
+              </VCol>
+
+              <!-- Posted On -->
+              <VCol cols="12">
+                <AppDateTimePicker
+                  v-model="PostedOn"
+                  label="Posted On"
+                  placeholder="YYYY-MM-DD"
+                />
+              </VCol>
+
+              <!-- Expires On -->
+              <VCol cols="12">
+                <AppDateTimePicker
+                  v-model="ExpiresOn"
+                  label="Expires On"
+                  placeholder="YYY-MM-DD"
+                />
+              </VCol>
+
+              <!-- Company -->
+              <VCol cols="12">
+                <AppAutocomplete
+                  v-model="CompanyId"
+                  label="Company"
+                  :items="companyOptions"
+                  item-title="name"
+                  item-value="id"
+                  :disabled="props.employeeData !== null"
+                />
+              </VCol>
+
+              <!-- Submit and Cancel Buttons -->
+              <VCol cols="12">
+                <VBtn type="submit" class="me-3">Submit</VBtn>
+                <VBtn
+                  variant="tonal"
+                  color="secondary"
+                  @click="closeNavigationDrawer"
+                  >Cancel</VBtn
+                >
+              </VCol>
+            </VRow>
+          </VForm>
+        </VCardText>
+      </VCard>
+    </PerfectScrollbar>
+  </VNavigationDrawer>
+</div>
+</template>
+
+<!-- Scoped styles -->
+<style scoped>
+.skills-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+}
+
+.skills-checkbox {
+  display: flex;
+  align-items: center;
+}
+
+.skills-checkbox input {
+  margin-right: 5px;
+}
+
+.skills-margin {
+  margin-bottom: 10px;
+}
+</style>
